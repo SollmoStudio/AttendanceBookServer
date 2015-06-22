6112a8d169de6e9f2052a0c276283242ceca3e79
ParkJuhyeong <majecty@gmail.com>
2015-06-22 13:07:56 +0900

    Remove unused codes from snap init.

--- src/Application.hs
+++ src/Application.hs
@@ -8,22 +8,15 @@ module Application where
 ------------------------------------------------------------------------------
 import Control.Lens
 import Snap.Snaplet
-import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
 import Snap.Snaplet.Session
 
 ------------------------------------------------------------------------------
 data App = App
-    { _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
+    {
+      _sess :: Snaplet SessionManager
     }
 
 makeLenses ''App
-
-instance HasHeist App where
-    heistLens = subSnaplet heist
-
 
 ------------------------------------------------------------------------------
 type AppHandler = Handler App App
