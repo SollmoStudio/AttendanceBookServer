d4f8cd3d3c5627d4550381fc1a39c18c367f3c06
ParkJuhyeong <majecty@gmail.com>
2015-06-22 13:27:43 +0900

    Implement makeUser protocol.

--- src/Site.hs
+++ src/Site.hs
@@ -10,9 +10,11 @@ module Site
 
 ------------------------------------------------------------------------------
 import Control.Applicative
+import Control.Monad.IO.Class
 import Data.ByteString (ByteString)
 import Data.Monoid
 import qualified Data.Text as T
+import Database.MySQL.Simple
 import Snap.Core
 import Snap.Snaplet
 import Snap.Snaplet.Session.Backends.CookieSession
@@ -23,8 +25,20 @@ import Application
 handleHello :: Handler App App ()
 handleHello = writeText "Hi"
 
+handleMakeUser :: Handler App App ()
+handleMakeUser = method POST $ do
+  email <- getParam "email"
+  password <- getParam "password"
+  conn <- liftIO $ connect defaultConnectInfo
+  liftIO $ execute conn (
+    "INSERT  attendance.user SET " `mappend`
+    "  email=?, password=?"
+    ) (email, password)
+  writeBS $ "ok"
+
 routes :: [(ByteString, Handler App App ())]
 routes = [ ("/hello", handleHello)
+         , ("/makeUser", handleMakeUser)
          , ("", serveDirectory "static")
          ]
 
