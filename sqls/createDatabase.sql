24388e1a6ba8d9e945454baa112cf6e99043af16
ParkJuhyeong <majecty@gmail.com>
2015-06-22 13:58:14 +0900

    Add attendance mysql table.

--- sqls/createDatabase.sql
+++ sqls/createDatabase.sql
@@ -7,3 +7,9 @@ CREATE TABLE `user` (
   UNIQUE KEY (email)
 ) ENGINE=InnoDB;
 
+CREATE TABLE `attendance` (
+  `email` varchar(90) NOT NULL,
+  `attendanceTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX ix_email (email),
+  FOREIGN KEY fx_email (email) REFERENCES `user` (email) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
